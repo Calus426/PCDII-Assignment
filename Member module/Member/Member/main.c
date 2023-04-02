a915faf5a41a24718f576b7e62139106b1fd6b89
Caslu426 <lucaschong426@gmail.com>
2023-04-02 21:02:36 +0800

    addmember
    complete but still need to check

--- Member module/Member/Member/main.c
+++ Member module/Member/Member/main.c
@@ -4,30 +4,12 @@
 #include <math.h>
 #pragma warning (disable:4996)
 
-typedef struct {
-	int day, month, year;
-}Date;
-
-typedef struct {
-	char add1[40], add2[40], postcode[6], city[40], state[40];
-		
-}Address;
-
-typedef struct
-{
-	char memberId[7],gender,memberPhone[12],uplineId[6];
-	Date joinDate;
-	Address memberAdd;
-}Member;
+void addMember();
 
 void main() 
 { 
-	FILE* ptr = fopen("member.bin", "rb");
+	
 
-	if (ptr == NULL)
-	{
-		printf("Unable to open file");
-		exit(-1);
-	}
+	
 }
 
