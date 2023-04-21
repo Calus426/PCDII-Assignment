d24a88b5c2802af4a595f18ee971d787f66ca07a
TKwong0338 <110614866+TKwong0338@users.noreply.github.com>
2023-04-21 22:28:07 +0800

    problem slove

--- Stock Module/Stock Module/Stock Module/main.c
+++ Stock Module/Stock Module/Stock Module/main.c
@@ -1,10 +1,10 @@
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<ctype.h>
 
 #pragma warning(disable:4996)
-
-void addNew();
 
 typedef struct
 {
@@ -13,15 +13,26 @@ typedef struct
 	double MPrice;
 	int MStock, MMinimum, MReorder;
 
-}NewMerchandise;
+}MerchandiseInStock;
 
-NewMerchandise NM;
+
+void addNew();
+void search(MerchandiseInStock MIS[], int* mDataSize);
+void merchandiseData(MerchandiseInStock MIS[], int* mDataSize);
+void displayData(MerchandiseInStock MIS[], int mDataSize);
+
 
 void main() {
 
-	int mChoice;
+	MerchandiseInStock MIS[100];
+	int mDataSize;
+	merchandiseData(MIS, &mDataSize);
+
+
+	int  mChoice;
 
 	do {
+
 
 		printf("\n\n================================\n"
 			"    What is Your Next Move??\n"
@@ -48,7 +59,8 @@ void main() {
 			break;
 
 		case 3:
-			
+			displayData(MIS, mDataSize);
+			search(MIS, mDataSize);
 			break;
 
 
@@ -83,52 +95,6 @@ void main() {
 	system("pause");
 
 }
-
-void addNew()
-{
-	FILE* AN;
-	AN = fopen("stock.txt", "a");
-
-	if (AN == NULL)
-	{
-		printf("Unable to open the file\n");
-		exit(-1);
-	}
-	char option;
-	do
-	{
-		printf("1. Enter New Merchandise Code              :");
-		scanf("%s", &NM.MCode);
-
-		printf("2. Enter New Merchandise Name              :");
-		scanf(" %[^\n]", &NM.MName);
-
-		printf("3. Enter New Merchandise Price             :");
-		scanf(" %lf", &NM.MPrice);
-
-		printf("4. Enter New Merchandise Stock             :");
-		scanf(" %d", &NM.MStock);
-
-		printf("5. Enter New Merchandise Minimum Level     :");
-		scanf(" %d", &NM.MMinimum);
-
-		printf("6. Enter New Merchandise Reorder Quantity  :");
-		scanf(" %d", &NM.MReorder);
-
-		fprintf(AN, "%s|%s|%.2lf|%d|%d|%d|", NM.MCode, NM.MName, NM.MPrice, NM.MStock, NM.MMinimum, NM.MReorder);
-
-		printf("\nAny New Merchandise More ? (if Yes press Y or No press N) :");
-		scanf(" %c", &option);
-
-
-	} while (toupper(option) != 'N');
-
-	printf("New merchandise saved \n");
-	system("pause");
-
-	fclose(AN);
-}
-
 
 
 
