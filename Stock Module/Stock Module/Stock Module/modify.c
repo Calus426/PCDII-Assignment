ff0f31cc667fa0917e6ffacceace810b31132338
TKwong0338 <110614866+TKwong0338@users.noreply.github.com>
2023-04-22 21:32:12 +0800

    modify 2

--- Stock Module/Stock Module/Stock Module/modify.c
+++ Stock Module/Stock Module/Stock Module/modify.c
@@ -12,21 +12,17 @@ typedef struct
 	int MStock, MMinimum, MReorder;
 
 }MerchandiseInStock;
-typedef struct
-{
-	char MCode[10];
-	char MName[30];
-	double MPrice;
-	int MStock, MMinimum, MReorder; 
-
-}ModifyMerchandiseData;
-
-ModifyMerchandiseData mmd;
 
 void modifyData(MerchandiseInStock MIS[], int *mDataSize)
 {
-	int option;
-	
+	int option,	selection;
+	int matchNumber;
+	char mMCode[10];
+	char mMName[30];
+	double mMPrice;
+	int mMStock, mMMinimum, mMReorder;
+	do
+	{
 
 	displayData(MIS, *mDataSize);
 	MerchandiseInStock matchData;
@@ -41,7 +37,7 @@ void modifyData(MerchandiseInStock MIS[], int *mDataSize)
 	{
 		if ((strcmp(MTSCode, MIS[i].MCode) == 0))
 		{
-			matchData = MIS[i];
+			matchNumber = i;
 		}
 
 	}
@@ -49,12 +45,12 @@ void modifyData(MerchandiseInStock MIS[], int *mDataSize)
 	printf("\n\n  =====================================================\n");
 	printf("                  < MERCHANDISE DETAIL >\n");
 	printf("  =====================================================\n");
-	printf("  MERCHANDISE CODE             : %s\n", matchData.MCode);
-	printf("  MERCHANDISE NAME             : %s\n", matchData.MName);
-	printf("  MERCHANDISE PRICE (RM)       : %.2lf\n", matchData.MPrice);
-	printf("  MERCHANDISE STOCK IN HAND    : %d\n", matchData.MStock);
-	printf("  MERCHANDISE MINIMUM LEVEL    : %d\n", matchData.MMinimum);
-	printf("  MERCHANDISE REORDER QUANTITY : %d\n", matchData.MReorder);
+	printf("  MERCHANDISE CODE             : %s\n",	MIS[matchNumber].MCode);
+	printf("  MERCHANDISE NAME             : %s\n",	MIS[matchNumber].MName);
+	printf("  MERCHANDISE PRICE (RM)       : %.2lf\n", MIS[matchNumber].MPrice);
+	printf("  MERCHANDISE STOCK IN HAND    : %d\n",	MIS[matchNumber].MStock);
+	printf("  MERCHANDISE MINIMUM LEVEL    : %d\n",	MIS[matchNumber].MMinimum);
+	printf("  MERCHANDISE REORDER QUANTITY : %d\n",	MIS[matchNumber].MReorder);
 	printf("  =====================================================\n\n");
 
 	printf("    Which data you will like to change \n");
@@ -73,29 +69,46 @@ void modifyData(MerchandiseInStock MIS[], int *mDataSize)
 	{
 	case 1 :
 			printf("Enter New Merchandise Code   :");
-			scanf(" %[^\n]", &mmd.MCode);
-		
+			scanf(" %[^\n]", &mMCode);
+			strcpy(MIS[matchNumber].MCode, mMCode);
 
+			FILE *MMD;
+			MMD = fopen("stock.txt", "w");
+
+			for (int k = 0; k < *mDataSize; k++)
+			{
+				fprintf(MMD, "%s|%s|%.2lf|%d|%d|%d|\n", MIS[k].MCode, MIS[k].MName, MIS[k].MPrice, MIS[k].MStock, MIS[k].MMinimum, MIS[k].MReorder);
+			}
 			break;
 
 	case 2:
-
+			printf("Enter New Merchandise Name   :");
+			scanf(" %[^\n]", &mMName);
+			strcpy(MIS[matchNumber].MName, mMName);
 			break;
 
 	case 3:
-
+			printf("Enter New Merchandise Price   :");
+			scanf(" %lf", &mMPrice);
+			MIS[matchNumber].MPrice = mMPrice;
 			break;
 
 	case 4:
-
+			printf("Enter New Merchandise Stock Quantity   :");
+			scanf(" %lf", &mMPrice);
+			MIS[matchNumber].MStock = mMStock;
 			break;
 
 	case 5:
-
+			printf("Enter New Merchandise Minimum Level   :");
+			scanf(" %lf", &mMPrice);
+			MIS[matchNumber].MMinimum = mMMinimum;
 			break;
 
 	case 6:
-
+			printf("Enter New Merchandise Reorder Quantity   :");
+			scanf(" %lf", &mMPrice);
+			MIS[matchNumber].MReorder = mMReorder;
 			break;
 	
 	default:
@@ -105,6 +118,9 @@ void modifyData(MerchandiseInStock MIS[], int *mDataSize)
 		printf("  ====================================\n\n");
 		break;
 	}
-	
 
+	printf("Press 'Y' if you want to change other merchandise press 'N' if you want to back to menu :");
+	scanf(" &d", &selection);
+	
+	} while (toupper(selection) != 'N');
 }
