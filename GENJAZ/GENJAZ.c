f2fba5a3d949c03386a7c65067a971da2ab93a22
JasonTan03 <tanyannshew@gmail.com>
2023-04-24 16:38:04 +0800

    Combined sales and member module

--- GENJAZ/GENJAZ.c
+++ GENJAZ/GENJAZ.c
@@ -1,31 +1,42 @@
 #include <stdio.h>
 #pragma warning (disable:4996)
 
+void memberModule();
+void SalesInformationModule();
+
 void main() {
 	int select = 0;
-	printf("\t+---------------------------------------------------------------+\n");
-	printf("\t  ___________   __________\n");
-	printf("\t  |==========|  |=========|\n");
-	printf("\t  |==|          |==|\n");
-	printf("\t  |==|          |==_______\n");
-	printf("\t  |==|     __   |=========|\n");
-	printf("\t  |==|     ==|  |==|\n");
-	printf("\t  |==______==|  |==_______\n");
-	printf("\t  |==========|  |=========|\n");
-	printf("\t  \n");
-	printf("\t+---------------------------------------------------------------+\n");
 
 	do {
-		printf("");
+		system("cls");
+		printf("\t+-------------------------------------------------------------------------------+\n");
+		printf("\t ___________   __________   ____    ___    ___________     _____    ___________\n");
+		printf("\t |==========|  |=========|  |===\\   |==|   |==========|   /=====\\   |==========|\n");
+		printf("\t |==|          |==|         |====\\  |==|       |==|      /== _ ==\\         /==/  \n");
+		printf("\t |==|          |==_______   |==|==\\ |==|       |==|      |==|_|==|       /==/    \n");
+		printf("\t |==|     __   |=========|  |==|\\==\\|==|       |==|      |=======|     /==/          \n");
+		printf("\t |==|     ==|  |==|         |==| \\== ==|   __  |==|      |==| |==|   /==/        \n");
+		printf("\t |==______==|  |==_______   |==|  \\====|  |==| |==|      |==| |==|  |==/_______\n");
+		printf("\t |==========|  |=========|  |==|   \\===|    |====|       |==| |==|  |==========|\n");
+		printf("\t  \n");
+		printf("\t+-------------------------------------------------------------------------------+\n");
+		printf("\n\t\t\t+====================================+");
+		printf("\n\t\t\t|\t 1) Staff  Information       |");
+		printf("\n\t\t\t|\t 2) Member Information       |");
+		printf("\n\t\t\t|\t 3) Sales  Information       |");
+		printf("\n\t\t\t|\t 4) Stock  Information       |");
+		printf("\n\t\t\t|\t 5)       Exit               |");
+		printf("\n\t\t\t+====================================+\n");
+		printf("\t\t\t Select An Option : ");
 		rewind(stdin);
 		scanf("%d", &select);
 
 		switch (select) {
 		case 1:
 			break;
-		case 2:
+		case 2: memberModule();
 			break;
-		case 3:
+		case 3: SalesInformationModule();
 			break;
 		case 4:
 			break;
